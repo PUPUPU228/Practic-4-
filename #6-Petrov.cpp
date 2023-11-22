bf08a09dccdee227f966fc0b5fef88dc3dc26ee1
PUPUPU228 <151516533+PUPUPU228@users.noreply.github.com>
2023-11-22 15:08:27 +0300

    Add files via upload

---
+++ #6-Petrov.cpp
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+namespace AgeToBirthYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваш возраст: "); int age = Convert.ToInt32(Console.ReadLine());
+            //Определяем текущий год
+            int currentYear = DateTime.Now.Year;
+            // Вычисляем год рождения
+            int birthYear = currentYear - age;
+            Console.WriteLine("Ваш год рождения: " + birthYear);
+            Console.ReadLine();
+        }
+    }
+}
